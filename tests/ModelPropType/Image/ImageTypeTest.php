8f57b3439727982e909b10dc8d8bd6ab7ed34838
Intbizth <im_bonbonn@hotmail.com>
2018-12-27 16:30:42 +0700

    use abstract test

--- tests/ModelPropType/Image/ImageTypeTest.php
+++ tests/ModelPropType/Image/ImageTypeTest.php
@@ -15,24 +15,14 @@ class ImageTypeTest extends AbstractPropTypeTest
             new ImageType('logo'),
         ]);
 
-        $allCodes = $this->manager->getCodes();
-
-        $this->assertCount(6, $allCodes);
-        $this->assertEquals(file_get_contents(__DIR__ . '/1ExpectedModel.php'),
-            $allCodes[$this->codeDir() . '/Mock.php']->getContent());
-        $this->assertEquals(file_get_contents(__DIR__ . '/1ExpectedInterface.php'),
-            $allCodes[$this->codeDir() . '/MockInterface.php']->getContent());
-
-        $this->assertEquals(file_get_contents(__DIR__ . '/1ExpectedModelImage.php'),
-            $allCodes[$this->codeDir() . '/MockImage.php']->getContent());
-
-        $this->assertEquals(file_get_contents(__DIR__ . '/1ExpectedInterfaceImage.php'),
-            $allCodes[$this->codeDir() . '/MockImageInterface.php']->getContent());
-
-        $this->assertEquals(file_get_contents(__DIR__ . '/1ExpectedDoctrine.orm.xml'),
-            $allCodes[$this->codeDir() . '/Mock.orm.xml']->getContent());
-
-        $this->assertEquals(file_get_contents(__DIR__ . '/1ExpectedDoctrineImage.orm.xml'),
-            $allCodes[$this->codeDir() . '/MockImage.orm.xml']->getContent());
+        $this
+            ->assertCountFilesWillBeCreated(6)
+            ->assertFileWillBeCreated($this->codeDir() . '/Mock.php', file_get_contents(__DIR__ . '/1ExpectedModel.php'))
+            ->assertFileWillBeCreated($this->codeDir() . '/MockInterface.php', file_get_contents(__DIR__ . '/1ExpectedInterface.php'))
+            ->assertFileWillBeCreated($this->codeDir() . '/MockImage.php', file_get_contents(__DIR__ . '/1ExpectedModelImage.php'))
+            ->assertFileWillBeCreated($this->codeDir() . '/MockImageInterface.php', file_get_contents(__DIR__ . '/1ExpectedInterfaceImage.php'))
+            ->assertFileWillBeCreated($this->codeDir() . '/Mock.orm.xml', file_get_contents(__DIR__ . '/1ExpectedDoctrine.orm.xml'))
+            ->assertFileWillBeCreated($this->codeDir() . '/MockImage.orm.xml', file_get_contents(__DIR__ . '/1ExpectedDoctrineImage.orm.xml'))
+        ;
     }
 }
